9110ab8641e3c472a7994331b8f9f82879a9b203
Robin <robin888b@outlook.com>
2022-06-09 23:24:21 +0200

    👀

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
+alert("Website under developpement, Embed system don't work")
+/*
+const WebhookLinkInput = document.querySelector("#WebhookLinkInput")
+WebhookLinkInput.addEventListener('input', function () {
+    const regex = /https?:\/\/(ptb\.)?(canary\.)?discord\.com\/api\/webhooks\/[0-9]+\/[A-Za-z0-9_]+/gm
 
-const loadItem = async () => {
-    
-}
+    // Alternative syntax using RegExp constructor
+    // const regex = new RegExp('https?:\\/\\/(ptb\\.)?discord\\.com\\/api\\/webhooks\\/[0-9]+\\/[A-Za-z0-9_]+', 'gm')
+
+    const str = WebhookLinkInput.value;
+    let m;
+
+    while ((m = regex.exec(str)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+        
+        // The result can be accessed through the `m`-variable.
+        m.forEach((match, groupIndex) => {
+            console.log(`Found match, group ${groupIndex}: ${match}`);
+        });
+    }
+});*/
 
 
 /*
@@ -16,18 +36,32 @@ fetch('https://ptb.discord.com/api/webhooks/981910278977515560/pHNojKrKTN3up54iy
 
 const webTestUrl = 'https://discord.com/api/webhooks/981910278977515560/pHNojKrKTN3up54iyUFomwYo-2WTSHQ-DGbSRNV_nzsC5bBGuVy-Omx1nFYiujJ_WqBs'
 
+function GEID(id){
+    return document.getElementById(id)
+}
 
 function sendHook(){
     let WebhookTextInput = document.getElementById("WebhookTextInput").value
     let url = document.getElementById("WebhookLinkInput").value
+    let customWhProfilCheck = document.getElementById("CustomWhProfilCheck")
+    if (customWhProfilCheck.classList.contains("checked")) {
+        var dataJson = {
+            "content": WebhookTextInput
+        }
+        let WhUsername = document.getElementById("WhNameInput").value //                    Nom du webhook
+        let WhPpUrlInput = document.getElementById("WhPpUrlInput").value//                  PP du webhook
+        if(WhUsername != "" | WhUsername != " "){dataJson.username = WhUsername}
+        if(WhPpUrlInput != "" | WhPpUrlInput != " "){dataJson.avatar_url = WhPpUrlInput}
+        var data = JSON.stringify(dataJson);
+    }else{
+        var data = JSON.stringify({
+            "content": WebhookTextInput
+        });
+    }
     xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json");
-	var data = JSON.stringify({
-        "content": WebhookTextInput,
-        "username": "username",
-        "avatar_url": "https://i.imgur.com/AfFp7pu.png",
-    });
+	
     console.log(data)
 	xhr.send(data);
 	xhr.onreadystatechange = function() {
@@ -44,4 +78,10 @@ function tgl1() {
     let WhProfilMenu = document.getElementById("customWhProfil")
     WhProfilchek.classList.toggle("checked")
     WhProfilMenu.classList.toggle("show")
+}
+
+
+function revealEmb(nb) {
+    let content = document.getElementById(`WhEmbUi${nb}`)
+    content.classList.toggle("active")
 }
